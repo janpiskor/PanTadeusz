748a7a738f512fcb652ff0bc1d00cbb144d80cf7
janpiskor <janpiskor1@gmail.com>
2017-11-06 19:41:41 +0100

    Pan Taduesz w PHP v2

--- index.php
+++ index.php
@@ -1,35 +1,50 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Pan Tadeusz</title>
-	<meta charset="utf-8"/>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
 </head>
 <body>
 
-<?php
+<header class="container">
+<h1>Pan Tadeusz czyli ostatni zjazd na Litwie. <br>Historia szlachecka z roku 1811 i 1812 we dwunastu księgach wierszem</h1>
+</header>
 
-	for($i=1; $i<=12; $i++){
-		echo "<li><a href='./index.php?ksiega=$i'>Ksiega $i</a></li>";
-}
+<div class = "container">
+	<div class="row">
+		<div class="col-md-3 col-xs-12">
+			<div class="list-group">
+			<?php 
+	
+			for($i=1; $i <=12; $i++)
+			{
+			echo "<a  href='./index.php?ksiega=$i' class='list-group-item list-group-item-action'>Księga $i</a>";
+			}
+			?>
+			</div>
+		</div>
+		
+		
+		<div class="col-md-9 col-xs-12">
+		<?php
+			if (!empty($_GET))
+			{
+			$k = $_GET['ksiega'];
+			include_once("./k$k.html");
+			}
+		?>
+		</div>
+	</div>
+</div>
 
-	if(!empty($_GET)){
-		$k = $_GET['ksiega'];
-		include_once("./k$k.html");
-	}
+<footer>(c) Jan Piskor </footer>
+	
+	
 
-?>
-<!--
-	<a href="./k1.html">Księga Pierwsza</a><br>
-	<a href="./k2.html">Księga Druga</a><br>
-	<a href="./k3.html">Księga Trzecia</a><br>
-	<a href="./k4.html">Księga Czwarta</a><br>
-	<a href="./k5.html">Księga Piąta</a><br>
-	<a href="./k6.html">Księga Szósta</a><br>
-	<a href="./k7.html">Księga Siódma</a><br>
-	<a href="./k8.html">Księga Ósma</a><br>
-	<a href="./k9.html">Księga Dziewiąta</a><br>
-	<a href="./k10.html">Księga Dziesiąta</a><br>
-	<a href="./k11.html">Księga Jedenasta</a><br>
-	<a href="./k12.html">Księga Dwunasta</a><br>
-	-->
 </body>
 </html>
